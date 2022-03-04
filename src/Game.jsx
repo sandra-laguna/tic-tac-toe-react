f0a7894e54f333625fddc9b8d6dcccdc92bb765f
sandra-laguna <idsandrax@gmail.com>
2022-03-04 22:00:31 +0100

    tic tac toe

---
+++ src/Game.jsx
@@ -0,0 +1,168 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rowStyle = {
+  display: "flex"
+};
+
+const squareStyle = {
+  width: "60px",
+  height: "60px",
+  backgroundColor: "#ddd",
+  margin: "4px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "20px",
+  color: "white"
+};
+
+const boardStyle = {
+  backgroundColor: "#eee",
+  width: "208px",
+  alignItems: "center",
+  justifyContent: "center",
+  display: "flex",
+  flexDirection: "column",
+  border: "3px #eee solid"
+};
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column"
+};
+
+const instructionsStyle = {
+  marginTop: "20px",
+  marginBottom: "5px",
+  fontWeight: "bold",
+  fontSize: "16px"
+};
+
+const buttonStyle = {
+  marginTop: "15px",
+  marginBottom: "16px",
+  width: "80px",
+  height: "40px",
+  backgroundColor: "#8acaca",
+  color: "white",
+  fontSize: "16px"
+};
+
+function Square(props) {
+  return (
+    <div className="square" style={squareStyle} onClick={props.onClick}>
+      {props.value}
+    </div>
+  );
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  resetButton(){
+    this.setState({
+      squares: [],
+      xIsNext: {}
+    });
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let statusWinner, statusPlayer;
+    if (winner) {
+      statusWinner = "🎉 Winner: " + winner +" 🎉";
+    } else {
+      statusPlayer = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div style={containerStyle} className="gameBoard">
+        <div id="statusArea" className="status" style={instructionsStyle}>
+          <span>{statusWinner==null?statusPlayer:statusWinner}</span>
+        </div>
+        <button style={buttonStyle} onClick={()=>this.resetButton()}>Reset</button>
+        <div style={boardStyle}>
+          <div className="board-row" style={rowStyle}>
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row" style={rowStyle}>
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row" style={rowStyle}>
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  componentDidMount(){
+    document.title = "Tic tac toe | React"
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Game;
